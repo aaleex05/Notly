b1e3ff7d779dd747fb6aa6caa295fc9927ce01c8
Alex Aperador Salcedo <alexapsa2007@gmail.com>
2025-11-13 21:32:17 +0100

    Update page.tsx

--- src/app/dashboard/folders/[id]/page.tsx
+++ src/app/dashboard/folders/[id]/page.tsx
@@ -9,21 +9,24 @@ import Button from "@/components/ui/buttonStyle"
 
 export default function FolderPage({ params }: { params: Promise<{ id: number }> }) {
     const { id } = use(params)
-    return (
-        <FolderPageContentClient id={id} />
-    )
+    return <FolderPageContentClient id={id} />
 }
 
 function FolderPageContentClient({ id }: { id: number }) {
 
+    // Tipo de cada tarea
     interface taskProps {
-        name: string,
-        description: string,
-        status: string,
-        priority: string,
-        done: boolean,
-        id: number,
+        name: string
+        description: string
+        status: string
+        priority: string
+        done: boolean
+        id: number
         expirationDate: null
+    }
+
+    interface FolderTaskRelation {
+        tasks: taskProps
     }
 
     const { FolderContent, folderContent, loading } = useFolder()
@@ -34,13 +37,15 @@ function FolderPageContentClient({ id }: { id: number }) {
 
     if (loading) {
         return (
-            <div className="flex items-center h-full justify-center"><Spinner /></div>
+            <div className="flex items-center h-full justify-center">
+                <Spinner />
+            </div>
         )
     }
 
-    const tasksToShow = folderContent[0]?.folder_tasks?.map(
-        (relation: any) => relation.tasks
-    ) || []
+    const tasksToShow: taskProps[] = (folderContent[0]?.folder_tasks?.map(
+        (relation: FolderTaskRelation) => relation.tasks
+    )) || []
 
     if (tasksToShow.length === 0) {
         return (
@@ -50,7 +55,7 @@ function FolderPageContentClient({ id }: { id: number }) {
                 </div>
                 <h1 className="text-2xl mb-2 font-semibold">No hay ninguna tarea en esta carpeta.</h1>
                 <CreateTask>
-                    <Button variant="white" size='default' className='flex gap-3 items-center text-md'>
+                    <Button variant="white" size="default" className="flex gap-3 items-center text-md">
                         Crear tarea
                     </Button>
                 </CreateTask>
@@ -60,14 +65,13 @@ function FolderPageContentClient({ id }: { id: number }) {
 
     return (
         <div>
-            <h1 className="text-3xl p-5 font-bold">Carpeta: {folderContent[0].name}</h1>
+            <h1 className="text-3xl p-5 font-bold">Carpeta: {folderContent[0]?.name}</h1>
 
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2.5 sm:gap-4 p-2.5 sm:p-5 auto-rows-fr'>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2.5 sm:gap-4 p-2.5 sm:p-5 auto-rows-fr">
                 {tasksToShow.map((task: taskProps) => (
                     <TaskCard key={task.id} {...task} />
                 ))}
             </div>
-
         </div>
     )
 }
